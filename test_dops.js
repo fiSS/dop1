f394883eadb93a94d50d2d6038a855e60599ea64
Filipp <fis_@mail.ru>
2019-05-06 22:24:46 +0300

    Update test_dops.js

--- test_dops.js
+++ test_dops.js
@@ -1,9 +1,71 @@
-'use strict';
-let num = 33721;
-num=num+"";
-num = num[0]*num[1]*num[2]*num[3]*num[4];
-console.log (num);
-num = (num**3);
-num = num+"";
-console.log(typeof num);
-document.write(num[0],' И ', num[1]);
+window.addEventListener('DOMContentLoaded', function() {              //load говорит js, что сейчас страница будет загружаться и только после того 
+                                                                    //как загрузиться полностью всё до последней картинки, js скрипт будет выполняться 
+'use strict';                                                        //DomContentLoaded это событие срабатывает, когда полностью загрузилась DOM структура документа
+let tab = document.querySelectorAll('.info-header-tab'),
+info = document.querySelector('.info-header'),
+tabContent = document.querySelectorAll('.info-tabcontent');
+
+
+function hideTabContent(a) {
+for(let i = a; i < tabContent.length; i++){
+tabContent[i].classList.remove('show');                 //удаляем класс show
+tabContent[i].classList.add('hide');                    //скроем все елементы tabContent со страницы
+}
+}
+
+hideTabContent("1");                                     //скрыват все tabContent кроме первого
+
+function showTabContent(b) {
+if (tabContent[b].classList.contains('hide')){
+tabContent[b].classList.remove('hide');                 //удаляем(скрываем) класс show
+tabContent[b].classList.add('show');
+}
+}
+
+info.addEventListener('click', function(event){
+let target = event.target;
+if(target && target.classList.contains('info-header-tab'))  {
+for(let i = 0; i < tab.length; i++) {                   //цикл будет работать до момента когда закончатся tab-ы
+if(target == tab[i]){
+hideTabContent(0);
+showTabContent(i);
+break;
+            }
+        }    
+    }
+});
+
+
+//modal узнать больше
+
+let more = document.querySelector('.more'),              //кнопка
+    overlay = document.querySelector('.overlay'),       //модальное окно
+    close = document.querySelector('.popup-close');     //крестик
+console.log(close);
+
+    more.addEventListener('click', function(){
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';        //запретим прокрутку страницы при открытии мод. Окна
+
+    });
+
+    close.addEventListener('click', function()  {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    })
+
+//modal  УЗНАТЬ ПОДРОБНЕЕ
+
+let descrBtn = document.querySelectorAll('.description-btn');
+    descrBtn.forEach(function(item) {
+        item.addEventListener('click', function(){
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hiden';
+        })
+        console.log(descrBtn);
+    });
+
+});
